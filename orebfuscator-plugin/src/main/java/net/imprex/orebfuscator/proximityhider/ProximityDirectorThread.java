089f15b168657e23ae61f578cbaa6c2c96c7b34a
Maurice Eisenblätter <code@ingrim4.me>
2023-06-05 15:42:14 +0200

    feat: wip - test phaser design

--- orebfuscator-plugin/src/main/java/net/imprex/orebfuscator/proximityhider/ProximityDirectorThread.java
+++ orebfuscator-plugin/src/main/java/net/imprex/orebfuscator/proximityhider/ProximityDirectorThread.java
@@ -1,5 +1,6 @@
 package net.imprex.orebfuscator.proximityhider;
 
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
@@ -7,10 +8,13 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Phaser;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.stream.IntStream;
 
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
@@ -19,6 +23,7 @@ import net.imprex.orebfuscator.Orebfuscator;
 
 public class ProximityDirectorThread extends Thread {
 
+	private final Phaser phaser = new Phaser(1);
 	private final CyclicBarrier cyclicBarrier;
 	private volatile boolean running = true;
 
@@ -34,7 +39,7 @@ public class ProximityDirectorThread extends Thread {
 	public ProximityDirectorThread(Orebfuscator orebfuscator) {
 		super(Orebfuscator.THREAD_GROUP, "ofc-proximity-director");
 
-		this.workerCount = 1;//orebfuscator.getOrebfuscatorConfig().advanced().proximityHiderThreads();
+		this.workerCount = 1;// orebfuscator.getOrebfuscatorConfig().advanced().proximityHiderThreads();
 		this.cyclicBarrier = new CyclicBarrier(this.workerCount);
 
 		this.worker = new ProximityWorker(orebfuscator);
@@ -64,14 +69,101 @@ public class ProximityDirectorThread extends Thread {
 		return this.running;
 	}
 
-	List<Player> getBucket() throws InterruptedException {
-		this.bucketLock.lock();
-		try {
-			this.nextBucket.await();
-			return bucketQueue.poll();
-		} finally {
-			this.bucketLock.unlock();
+	public static void main(String[] args) {
+		final Phaser phaser = new Phaser(1) {
+			@Override
+			protected boolean onAdvance(int phase, int registeredParties) {
+				System.out.println("onAdvance: " + phase + " " + registeredParties + " " + Thread.currentThread().getName());
+				if (registeredParties == 0) {
+					new Exception().printStackTrace();
+				}
+				return registeredParties == 0;
+			}
+		};
+
+		final Lock bucketLock = new ReentrantLock();
+		final Condition nextBucket = bucketLock.newCondition();
+
+		final Queue<String> queue = new ConcurrentLinkedQueue<>();
+		final int workerCount = Runtime.getRuntime().availableProcessors();
+
+		IntStream.range(0, workerCount).forEach(index -> {
+			new Thread(() -> {
+				while (!phaser.isTerminated()) {
+					try {
+						String element;
+
+						bucketLock.lock();
+						try {
+							nextBucket.await();
+							element = queue.poll();
+						} finally {
+							bucketLock.unlock();
+						}
+
+						System.out.println(Thread.currentThread().getName() + ": " + element);
+
+						phaser.arriveAndDeregister();
+					} catch (InterruptedException e) {
+						break;
+					}
+				}
+			}, "wroker-" + index).start();
+		});
+
+		while (!phaser.isTerminated()) {
+			try {
+				int bucketCount = (int) Math.round(Math.random() * workerCount);
+				if (bucketCount == 0) {
+					continue;
+				}
+
+				String element;
+
+				bucketLock.lock();
+				try {
+					for (int i = 0; i < bucketCount; i++) {
+						queue.offer("element " + i + " in phase " + (phaser.getPhase() + 1));
+					}
+				} finally {
+					bucketLock.unlock();
+				}
+
+				phaser.arriveAndAwaitAdvance();
+
+				Thread.sleep(1L);
+
+				if (bucketCount > 1) {
+					phaser.bulkRegister(bucketCount - 1);
+				}
+				System.out.println(String.format("bucketCount: %s, phase: %s, registed: %s, arrived: %s, queue: %s, time: %s", bucketCount,
+							phaser.getPhase(), phaser.getRegisteredParties(), phaser.getArrivedParties(), queue.size(), LocalTime.now().toString()));
+
+				bucketLock.lock();
+				try {
+					element = queue.poll();
+					for (int i = 0; i < bucketCount - 1; i++) {
+						nextBucket.signal();
+					}
+				} finally {
+					bucketLock.unlock();
+				}
+
+				System.out.println(Thread.currentThread().getName() + ": " + element);
+			} catch (Exception e) {
+				break;
+			}
 		}
+	}
+
+	List<Player> getBucket() throws InterruptedException {
+		List<Player> bucket;
+
+		while ((bucket = this.bucketQueue.poll()) == null) {
+			this.phaser.awaitAdvance(this.phaser.arriveAndDeregister());
+		}
+
+		return bucket;
 	}
 
 	void awaitNextExecution() throws InterruptedException, BrokenBarrierException {
@@ -90,7 +182,7 @@ public class ProximityDirectorThread extends Thread {
 				int bucketSize = (int) Math.ceil((float) playerCount / (float) bucketCount);
 
 				List<Player> localBucket = null;
-				
+
 				this.bucketLock.lock();
 				try {
 					// refill buckets
